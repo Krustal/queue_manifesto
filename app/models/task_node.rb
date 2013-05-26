b54c69b23dc17d5a9ad390087dd045f867ff1c53
Byron McMullen <byronsm@gmail.com>
2013-05-25 19:43:01 -0700

    cleaned up recursive relationship for task_nodes

--- app/models/task_node.rb
+++ app/models/task_node.rb
@@ -1,3 +1,5 @@
 class TaskNode < ActiveRecord::Base
   belongs_to :task_queue
+  belongs_to :next_node, class_name: "TaskNode", foreign_key: 'next_node'
+  has_one :parent, class_name: "TaskNode", foreign_key: 'next_node'
 end
--- app/models/task_queue.rb
+++ app/models/task_queue.rb
@@ -11,7 +11,7 @@ class TaskQueue < ActiveRecord::Base
       end
     else
       old_back = self.task_nodes.find(self.back)
-      old_back.next_node = task_node.id
+      old_back.next_node = task_node
       self.back = task_node.id
       if old_back.save && save
         return true
@@ -23,12 +23,12 @@ class TaskQueue < ActiveRecord::Base
   end
 
   def dequeue(task_node)
-    parent_node = begin 
-      self.task_nodes.where(next_node: task_node.id).limit(1).first
+    parent_node = begin
+      task_node.parent
     rescue
       nil
     end
-    next_node_id = task_node.next_node
+    next_node_id = task_node.next_node ? task_node.next_node.id : nil
     # set the queue front pointer to this element's next node
     # if it has one.
     if self.front == task_node.id
@@ -41,7 +41,7 @@ class TaskQueue < ActiveRecord::Base
     end
     # connect this nodes parent and child if they exist
     if parent_node
-      parent_node.next_node = next_node_id
+      parent_node.next_node = task_node.next_node
     end
     # save our changes to the records effected on the side
     if self.save! && (parent_node.nil? || parent_node.save!)

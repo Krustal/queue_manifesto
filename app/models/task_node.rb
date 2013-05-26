10ed64a961dcf9aae5fc60b59f7de763034f540f
Byron McMullen <byronsm@gmail.com>
2013-05-25 20:33:22 -0700

    added a depth to the descendents method, default of 4

--- app/models/task_node.rb
+++ app/models/task_node.rb
@@ -3,9 +3,9 @@ class TaskNode < ActiveRecord::Base
   belongs_to :next_node, class_name: "TaskNode", foreign_key: 'next_node'
   has_one :parent, class_name: "TaskNode", foreign_key: 'next_node'
 
-  def self_and_descendents
-    if next_node
-      ([self] << next_node.self_and_descendents).flatten
+  def self_and_descendents(depth: 4)
+    if next_node && depth > 1
+      ([self] << next_node.self_and_descendents(depth: depth - 1)).flatten
     else
       return self
     end
